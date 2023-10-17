0942f53873a35b5e012eeb960dc5db834521d9a5
Christopher Burg <ChristopherBu@creativedc.com>
2023-10-17 16:19:50 -0400

    Add DOCX, TXT, and MSG document support

--- src/features/chat/chat-services/chat-document-service.ts
+++ src/features/chat/chat-services/chat-document-service.ts
@@ -11,6 +11,8 @@ import { Document } from "langchain/document";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { nanoid } from "nanoid";
+import * as mammoth from "mammoth";
+import MsgReader from "@kenjiuno/msgreader";
 import {
   CHAT_DOCUMENT_ATTRIBUTE,
   ChatDocumentModel,
@@ -19,7 +21,32 @@ import {
 
 const MAX_DOCUMENT_SIZE = 100000000;
 
-const ALLOWED_FILE_TYPE_SET = ["application/pdf", "image/jpeg", "image/png"];
+const FORM_RECOGNIZER_EXTRACTION_MIME_TYPE_SET = [
+	"application/pdf",
+	"image/jpeg",
+	"image/png",
+];
+
+const MAMMOTH_EXTRACTION_MIME_TYPE_SET = [
+	"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+	"application/msword",
+];
+
+const TEXT_EXTRACTION_MIME_TYPE_SET = [
+	"text/plain",
+];
+
+const MSGREADER_EXTRACTION_MIME_TYPE_SET = [
+	"application/vnd.ms-outlook",
+
+	//	Hack because the browser fails to detect the correct MIME type for msg files
+	"application/octet-stream",
+];
+
+const ALLOWED_MIME_TYPE_SET = FORM_RECOGNIZER_EXTRACTION_MIME_TYPE_SET
+	.concat(MAMMOTH_EXTRACTION_MIME_TYPE_SET)
+	.concat(TEXT_EXTRACTION_MIME_TYPE_SET)
+	.concat(MSGREADER_EXTRACTION_MIME_TYPE_SET);
 
 export const UploadDocument = async (formData: FormData) => {
   const { docs, file, chatThreadId } = await LoadFile(formData);
@@ -40,23 +67,64 @@ const LoadFile = async (formData: FormData) => {
 		errorMessage = "Unable to load file. Please try again or contact ApplicationDevelopment@CreativeDC.com for assistance.";
 	} else if (file.size > MAX_DOCUMENT_SIZE) {
 		errorMessage = "File size is too large. Please upload a file less than 100MB.";
-	} else if (!ALLOWED_FILE_TYPE_SET.includes(file.type)) {
-		errorMessage = "Invalid file type. Only PDF, JPG, and PNG files are supported.";
+	} else if (!ALLOWED_MIME_TYPE_SET.includes(file.type)) {
+		errorMessage = "Invalid file type. Currently only DOCX, TXT, MSG, PDF, JPG, and PNG files are supported. Have a different file type? Contact ApplicationDevelopment@CreativeDC.com and let us know.";
 	}
 
 	try
 	{
 		if (!errorMessage) {
-			const client = initDocumentIntelligence();
 
-			const blob = new Blob([file], { type: file.type });
+			if (MAMMOTH_EXTRACTION_MIME_TYPE_SET.includes(file.type)) {
+				const arrayBuffer = await file.arrayBuffer();
+				const buffer = Buffer.from(arrayBuffer);
+				const result = await mammoth.extractRawText({ buffer });
+				
+				paragraphs = result
+					.value
+					?.split("\n")
+					?.filter((paragraph) => paragraph.trim() !== "");
 
-			const poller = await client.beginAnalyzeDocument(
-				"prebuilt-document",
-				await blob.arrayBuffer()
-			);
+			} else if (FORM_RECOGNIZER_EXTRACTION_MIME_TYPE_SET.includes(file.type)) {
+				const client = initDocumentIntelligence();
 
-			paragraphs = (await poller.pollUntilDone()).paragraphs;
+				const blob = new Blob([file], { type: file.type });
+
+				const poller = await client.beginAnalyzeDocument(
+					"prebuilt-document",
+					await blob.arrayBuffer()
+				);
+
+				paragraphs = (await poller.pollUntilDone())
+					.paragraphs
+					?.map((paragraph) => paragraph.content);
+
+			} else if (TEXT_EXTRACTION_MIME_TYPE_SET.includes(file.type)) {
+				const text = await file.text();
+				paragraphs = text
+					?.split("\n")
+					?.filter((paragraph) => paragraph.trim() !== "");
+
+			} else if (MSGREADER_EXTRACTION_MIME_TYPE_SET.includes(file.type)) {
+				const arrayBuffer = await file.arrayBuffer();
+				const buffer = Buffer.from(arrayBuffer);
+				const msgReader = new MsgReader(buffer);
+				const emailData = await msgReader.getFileData();
+
+				//	Add message metadata along with body
+				paragraphs = [
+					"From: " + emailData.senderName + " <" + emailData.senderEmail + ">"
+					, "To: " + emailData.recipients
+					, "Subject: " + emailData.subject
+					, "Date: " + emailData.messageDeliveryTime
+				]
+					.concat(emailData
+						.body
+						?.split("\n")
+						?.filter((paragraph) => paragraph.trim() !== "") 
+						?? []
+					);
+			}
 
 			if (!paragraphs || paragraphs.length === 0) {
 				errorMessage = "Unable to find any text for processing in this file.";
@@ -65,7 +133,7 @@ const LoadFile = async (formData: FormData) => {
 			if (paragraphs && !errorMessage) {
 				for (const paragraph of paragraphs) {
 					const doc: Document = {
-						pageContent: paragraph.content,
+						pageContent: paragraph,
 						metadata: {
 							file: file.name,
 						},
